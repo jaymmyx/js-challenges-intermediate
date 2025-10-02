55707f9ec07ca7bfe23eb134f93b08600afb6d6e
James Njoroge <developerjaymmy@gmail.com>
2025-10-02 08:31:25 +0300

    03-calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,18 @@
-function calculator() {}
+function calculator(num1, num2, operation) {
+  switch (operation) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+    case "%":
+      return num1 % num2;
+    default:
+      return -1;
+  }
+}
 
 module.exports = calculator;
