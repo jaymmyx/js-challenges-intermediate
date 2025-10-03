a4790af16c03b4a2e1f5c6f1e315e6bc5f2f10a4
James Njoroge <developerjaymmy@gmail.com>
2025-10-03 17:01:20 +0300

    16-are-all-chars-unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,17 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const strSet = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (strSet.has(char)) {
+      return false;
+    }
+
+    strSet.add(char);
+  }
+
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
