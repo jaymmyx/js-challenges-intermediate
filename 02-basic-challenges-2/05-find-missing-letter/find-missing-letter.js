e562b9a897cd01f4db7483656cda0eaaaeb4d9e9
James Njoroge <developerjaymmy@gmail.com>
2025-10-03 16:17:40 +0300

    15-find-missing-letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,16 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const alphabetArr = alphabet.split("");
+
+  const startLetterIdx = alphabetArr.indexOf(arr[0]);
+  const lastLetterIdx = alphabetArr.indexOf(arr[arr.length - 1]);
+
+  const expectedArr = alphabetArr.slice(startLetterIdx, lastLetterIdx);
+  for (let i = 0; i < expectedArr.length; i++) {
+    if (!arr.includes(expectedArr[i])) {
+      return expectedArr[i];
+    }
+  }
+}
 
 module.exports = findMissingLetter;
