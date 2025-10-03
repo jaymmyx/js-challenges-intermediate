eb580406bd1e3b2350d379b155e75e271c026096
James Njoroge <developerjaymmy@gmail.com>
2025-10-03 16:38:34 +0300

    15-find-missing-letter(charCode method)

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,16 +1,27 @@
+// function findMissingLetter(arr) {
+//   const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   const alphabetArr = alphabet.split("");
+
+//   const startLetterIdx = alphabetArr.indexOf(arr[0]);
+//   const lastLetterIdx = alphabetArr.indexOf(arr[arr.length - 1]);
+
+//   const expectedArr = alphabetArr.slice(startLetterIdx, lastLetterIdx);
+//   for (let i = 0; i < expectedArr.length; i++) {
+//     if (!arr.includes(expectedArr[i])) {
+//       return expectedArr[i];
+//     }
+//   }
+// }
+
 function findMissingLetter(arr) {
-  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const alphabetArr = alphabet.split("");
+  const charCodeStart = arr[0].charCodeAt(0);
 
-  const startLetterIdx = alphabetArr.indexOf(arr[0]);
-  const lastLetterIdx = alphabetArr.indexOf(arr[arr.length - 1]);
-
-  const expectedArr = alphabetArr.slice(startLetterIdx, lastLetterIdx);
-  for (let i = 0; i < expectedArr.length; i++) {
-    if (!arr.includes(expectedArr[i])) {
-      return expectedArr[i];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].charCodeAt(0) !== charCodeStart + i) {
+      return String.fromCharCode(charCodeStart + i);
     }
   }
+  return null; // If no letter is missing
 }
 
 module.exports = findMissingLetter;
