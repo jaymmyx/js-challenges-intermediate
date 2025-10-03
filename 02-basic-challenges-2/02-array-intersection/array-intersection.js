3a93a533f0d9c011041dd9eca2c8002d4bc0e848
James Njoroge <developerjaymmy@gmail.com>
2025-10-03 14:50:38 +0300

    12-array-intersection(set method)

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,13 +1,26 @@
-function arrayIntersection(arr1, arr2) {
-  const arr = [];
+// function arrayIntersection(arr1, arr2) {
+//   const arr = [];
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr2.includes(arr1[i])) {
-      arr.push(arr1[i]);
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr2.includes(arr1[i]) && !arr.includes(arr1[i])) {
+//       arr.push(arr1[i]);
+//     }
+//   }
+
+//   return arr;
+// }
+
+const arrayIntersection = (arr1, arr2) => {
+  const intersectionArr = [];
+  const set1 = new Set(arr1);
+
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      intersectionArr.push(num);
     }
   }
 
-  return arr;
-}
+  return intersectionArr;
+};
 
 module.exports = arrayIntersection;
