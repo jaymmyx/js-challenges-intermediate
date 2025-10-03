1fbf6b6f8f3883384507892965247d1c7f47cd5a
James Njoroge <developerjaymmy@gmail.com>
2025-10-03 15:43:01 +0300

    14-find-missing-number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,20 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (!arr) {
+    return undefined;
+  } else if (arr.length === 0) {
+    return 1;
+  } else {
+    // Sum from 1 upto n (inclusive)
+    const n = arr.length + 1;
+    const expectedSum = (n * (n + 1)) / 2;
+    let actualSum = 0;
+
+    for (let num of arr) {
+      actualSum += num;
+    }
+
+    return expectedSum - actualSum;
+  }
+}
 
 module.exports = findMissingNumber;
