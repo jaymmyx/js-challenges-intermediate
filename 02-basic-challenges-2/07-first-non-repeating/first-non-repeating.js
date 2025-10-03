b1dfd2ba69fdd8e6e05a7484bcd2188dd1f27d9d
James Njoroge <developerjaymmy@gmail.com>
2025-10-03 18:04:07 +0300

    07-first-non-repeating

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,19 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  // Have to finish loop so O(n) because what if first character is repeated at last index?
+
+  checkerObj = {};
+
+  for (const char of str) {
+    checkerObj[char] = (checkerObj[char] || 0) + 1;
+  }
+
+  for (const char of str) {
+    if (checkerObj[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
