4ccca26bf55cccf6fb565ce68e5740ef3088848a
James Njoroge <developerjaymmy@gmail.com>
2025-10-03 07:05:35 +0300

    09-count-vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,33 @@
-function countVowels() {}
+function countVowels(str) {
+  const formattedStr = str.toLowerCase();
+  let count = 0;
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    if (isVowel(formattedStr[i])) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+function isVowel(char) {
+  let vowelBool = true;
+  switch (char) {
+    case "a":
+      break;
+    case "e":
+      break;
+    case "i":
+      break;
+    case "o":
+      break;
+    case "u":
+      break;
+    default:
+      vowelBool = false;
+  }
+  return vowelBool;
+}
 
 module.exports = countVowels;
