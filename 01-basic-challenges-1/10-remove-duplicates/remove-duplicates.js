895c5474cd576ff2ba35266c67f08545dcaa2e5c
James Njoroge <developerjaymmy@gmail.com>
2025-10-03 07:19:15 +0300

    10-remove-duplicates

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,21 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  let noDuplicatesArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let isUnique = true;
+
+    for (let j = 0; j < noDuplicatesArr.length; j++) {
+      if (arr[i] === noDuplicatesArr[j]) {
+        isUnique = false;
+      }
+    }
+
+    if (isUnique) {
+      noDuplicatesArr.push(arr[i]);
+    }
+  }
+
+  return noDuplicatesArr;
+}
 
 module.exports = removeDuplicates;
