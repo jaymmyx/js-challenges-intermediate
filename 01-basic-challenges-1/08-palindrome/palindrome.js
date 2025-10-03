f2d4ebddb45b19d45b6ac76a74a01497baec0fad
James Njoroge <developerjaymmy@gmail.com>
2025-10-03 06:43:39 +0300

    08-palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,33 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  formattedStr = removeNonAlphaNumeric(str.toLowerCase());
+  return reverseString(formattedStr) === formattedStr;
+}
+
+function isAlphaNumeric(char) {
+  let unicode = char.charCodeAt(0);
+  return (unicode >= 48 && unicode <= 57) || (unicode >= 97 && unicode <= 122);
+}
+
+function removeNonAlphaNumeric(str) {
+  formattedStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (isAlphaNumeric(str[i])) {
+      formattedStr += str[i];
+    }
+  }
+
+  return formattedStr;
+}
+
+function reverseString(str) {
+  reversedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+
+  return reversedStr;
+}
 
 module.exports = isPalindrome;
