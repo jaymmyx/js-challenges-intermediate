2c56a7f329a3b9d5271e2b9c3b1122f01a50ad11
James Njoroge <developerjaymmy@gmail.com>
2025-10-04 20:46:47 +0300

    18-dice-game

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,27 @@
-function diceGameSimulation() {}
+function rollDie() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function diceGameSimulation(numSimulations) {
+  const results = [];
+
+  for (let i = 0; i < numSimulations; i++) {
+    const dice1 = rollDie();
+    const dice2 = rollDie();
+    const sum = dice1 + dice2;
+
+    let result = "";
+    if (sum === 7 || sum === 11) {
+      result = "win";
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = "lose";
+    } else {
+      result = "roll again";
+    }
+    results.push({ dice1, dice2, sum, result });
+  }
+
+  return results;
+}
 
 module.exports = diceGameSimulation;
