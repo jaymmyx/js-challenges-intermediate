55fc36aa2affce908ffab8581fe543b822d6a1db
James Njoroge <developerjaymmy@gmail.com>
2025-10-04 20:56:50 +0300

    19-format-phone-number

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,8 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+  const areaCode = arr.slice(0, 3).join("");
+  const centralOfficeCode = arr.slice(3, 6).join("");
+  const lineNumber = arr.slice(6).join("");
+  return `(${areaCode}) ${centralOfficeCode}-${lineNumber}`;
+}
 
 module.exports = formatPhoneNumber;
