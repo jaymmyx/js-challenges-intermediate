d8e30abd9119d4555252f812fdab06f9a8b0232f
James Njoroge <developerjaymmy@gmail.com>
2025-10-05 21:24:20 +0300

    22-calculate-total-sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,14 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const totalSales = products.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
+
+  const taxAmount = (totalSales * taxRate) / 100;
+
+  const totalSalesWithTax = totalSales + taxAmount;
+
+  return parseFloat(totalSalesWithTax.toFixed(2));
+}
 
 module.exports = calculateTotalSalesWithTax;
