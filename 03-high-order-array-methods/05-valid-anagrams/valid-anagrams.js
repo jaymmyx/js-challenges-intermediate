53d3e3fb77bd1ffe749aef07d5ec10197fe0d5a0
James Njoroge <developerjaymmy@gmail.com>
2025-10-05 22:50:52 +0300

    24-valid-anagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,22 @@
-function validAnagrams() {}
+function validAnagrams(word1, word2) {
+  // Early exit for different lengths - anagrams must have same length
+  if (word1.length !== word2.length) {
+    return false;
+  }
+
+  const charCount = {};
+
+  // Single pass: increment for word1, decrement for word2
+  for (let i = 0; i < word1.length; i++) {
+    const char1 = word1[i];
+    const char2 = word2[i];
+
+    charCount[char1] = (charCount[char1] || 0) + 1;
+    charCount[char2] = (charCount[char2] || 0) - 1;
+  }
+
+  // If strings are anagrams, all character counts should be zero
+  return Object.values(charCount).every((count) => count === 0);
+}
 
 module.exports = validAnagrams;
