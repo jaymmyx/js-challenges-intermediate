0a5e854369d3a1b5d883432a7ff6a4518cb1915b
James Njoroge <developerjaymmy@gmail.com>
2025-10-05 21:59:03 +0300

    23-highest-scoring-word

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,27 @@
-function highestScoringWord() {}
+function highestScoringWord(words) {
+  const wordsArr = words.split(" ");
+
+  const wordScores = wordsArr.map((word) => {
+    let score = 0;
+
+    for (let i = 0; i < word.length; i++) {
+      score += word[i].charCodeAt(0) - 96;
+    }
+
+    return score;
+  });
+
+  let highestScore = 0;
+  let highestIndex = 0;
+
+  for (let i = 0; i < wordScores.length; i++) {
+    if (wordScores[i] > highestScore) {
+      highestScore = wordScores[i];
+      highestIndex = i;
+    }
+  }
+
+  return wordsArr[highestIndex];
+}
 
 module.exports = highestScoringWord;
