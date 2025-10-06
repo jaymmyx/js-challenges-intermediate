e9096e14e75e7876ba95f8bb79313c0db10ef35e
James Njoroge <developerjaymmy@gmail.com>
2025-10-06 22:54:27 +0300

    36-array-sum

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,11 @@
-function arraySum() {}
+function arraySum(nums) {
+  if (nums.length === 1) {
+    return nums[0];
+  } else if (nums.length === 0) {
+    return 0;
+  }
+
+  return nums[nums.length - 1] + arraySum(nums.slice(0, nums.length - 1));
+}
 
 module.exports = arraySum;
