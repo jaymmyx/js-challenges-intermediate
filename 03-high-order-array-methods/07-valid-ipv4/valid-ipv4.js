6dc259df0d8c9248d060195176ea9dba74462ea7
James Njoroge <developerjaymmy@gmail.com>
2025-10-06 07:58:13 +0300

    26-valid-ipv4

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,15 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  const octets = str.split(".");
+
+  if (octets.length !== 4) {
+    return false;
+  }
+
+  return octets.every((octet) => {
+    const octetInt = parseInt(octet);
+
+    return octetInt >= 0 && octetInt <= 255 && octet === octetInt.toString();
+  });
+};
 
 module.exports = isValidIPv4;
