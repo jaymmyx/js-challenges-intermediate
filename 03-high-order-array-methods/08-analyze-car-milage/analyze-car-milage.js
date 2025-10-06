1fcbd202879160243c207747384510f75cfe3c52
James Njoroge <developerjaymmy@gmail.com>
2025-10-06 08:46:11 +0300

    27-analyze-car-mileage

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,34 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const highestMileageCar = cars.reduce((highest, car) => {
+    if (highest.mileage < car.mileage) {
+      return car;
+    } else {
+      return highest;
+    }
+  }, cars[0]);
+
+  const lowestMileageCar = cars.reduce((lowest, car) => {
+    if (car.mileage < lowest.mileage) {
+      return car;
+    } else {
+      return lowest;
+    }
+  }, cars[0]);
+
+  const totalMileage = cars.reduce((total, car) => {
+    return total + car.mileage;
+  }, 0);
+
+  const averageMileage = totalMileage / cars.length;
+
+  const averageObj = {
+    averageMileage: parseFloat(averageMileage.toFixed(2)),
+    highestMileageCar: highestMileageCar,
+    lowestMileageCar: lowestMileageCar,
+    totalMileage: parseFloat(totalMileage.toFixed(2)),
+  };
+
+  return averageObj;
+}
 
 module.exports = analyzeCarMileage;
