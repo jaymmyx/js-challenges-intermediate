99c442d1b3801aef74b88050f162223eb0806333
James Njoroge <developerjaymmy@gmail.com>
2025-10-06 18:08:24 +0300

    29-find-missing-letter-refactor

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,18 @@
-function findMissingLetter() {}
+function findMissingLetter(chars) {
+  // ['a', 'b', 'c', 'd', 'f'] => 'e'
+
+  const charCodes = chars.map((char) => char.charCodeAt(0));
+  let start = charCodes[0];
+
+  const missingCode = charCodes.find((charCode) => {
+    if (charCode - start > 1) {
+      return true;
+    }
+    start = charCode;
+    return false;
+  });
+
+  return missingCode ? String.fromCharCode(missingCode - 1) : false;
+}
 
 module.exports = findMissingLetter;
