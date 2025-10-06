9bcbd98216e3523342a28e84b40fa8f65ce4a93c
James Njoroge <developerjaymmy@gmail.com>
2025-10-06 07:37:45 +0300

    25-hashtag-generator

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,19 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.trim().length === 0 || str.trim().length > 140) {
+    return false;
+  }
+
+  const hashTag = str
+    .toLowerCase()
+    .trim()
+    .split(/\s+/)
+    .map((word) => {
+      word = word[0].toUpperCase() + word.slice(1);
+      return word;
+    })
+    .join("");
+
+  return `#${hashTag}`;
+}
 
 module.exports = generateHashtag;
