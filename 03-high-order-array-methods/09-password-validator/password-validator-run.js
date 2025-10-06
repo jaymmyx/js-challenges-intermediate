02d58a1d0afcee2687fbd128d23ffb88bd81c87b
James Njoroge <developerjaymmy@gmail.com>
2025-10-06 09:36:16 +0300

    28-password-validator

--- 03-high-order-array-methods/09-password-validator/password-validator-run.js
+++ 03-high-order-array-methods/09-password-validator/password-validator-run.js
@@ -4,3 +4,4 @@ const result1 = validatePassword('Abc12345');
 const result2 = validatePassword('password');
 
 console.log(result1, result2);
+
--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,32 @@
-function validatePassword() {}
+function validatePassword(password) {
+  // Early exit for length check
+  if (password.length < 8) {
+    return false;
+  }
+
+  let hasUpper = false;
+  let hasLower = false;
+  let hasDigit = false;
+
+  // Single pass through string
+  for (let i = 0; i < password.length; i++) {
+    const char = password[i];
+
+    if (char >= "A" && char <= "Z") {
+      hasUpper = true;
+    } else if (char >= "a" && char <= "z") {
+      hasLower = true;
+    } else if (char >= "0" && char <= "9") {
+      hasDigit = true;
+    }
+
+    // Early exit when all conditions met
+    if (hasUpper && hasLower && hasDigit) {
+      return true;
+    }
+  }
+
+  return hasUpper && hasLower && hasDigit;
+}
 
 module.exports = validatePassword;
