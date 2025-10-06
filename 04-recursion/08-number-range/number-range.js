fdd81d839ba0270174aa88dfcd84f5521e6c1689
James Njoroge <developerjaymmy@gmail.com>
2025-10-06 23:14:15 +0300

    37-number-range

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,9 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+  if (startNum === endNum) {
+    return [startNum];
+  }
+
+  return numberRange(startNum, endNum - 1).concat([endNum]);
+}
 
 module.exports = numberRange;
