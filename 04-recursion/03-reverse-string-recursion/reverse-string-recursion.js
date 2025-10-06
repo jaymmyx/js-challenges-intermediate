febbe8b4752a50a5fad68112e0790cd81e135e70
James Njoroge <developerjaymmy@gmail.com>
2025-10-06 22:05:55 +0300

    32-reverse-string-recursion

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(str) {
+  if (str === "") {
+    return "";
+  }
+
+  return reverseString(str.substring(1)) + str[0];
+}
 
 module.exports = reverseString;
