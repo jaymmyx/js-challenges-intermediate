b1357a26cecf911b8efb2bfce6000f382e956221
James Njoroge <developerjaymmy@gmail.com>
2025-10-07 07:38:11 +0300

    38-flatten-array

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,16 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  const result = [];
+
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      // Use spread operator for efficient concatenation
+      result.push(...flattenArray(item));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
 
 module.exports = flattenArray;
