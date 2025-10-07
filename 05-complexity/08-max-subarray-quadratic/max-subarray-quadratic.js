d34b3270a037732d9eeb86d5c40dbc8d90d7d015
James Njoroge <developerjaymmy@gmail.com>
2025-10-07 23:48:13 +0300

    40-max-subarray-quadratic

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,16 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  if (k > arr.length) return null;
+  let maxSum = -Infinity;
+  for (let i = 0; i <= arr.length - k; i++) {
+    let currentSum = 0;
+    for (let j = i; j < i + k; j++) {
+      currentSum += arr[i + j];
+    }
+    if (currentSum > maxSum) {
+      maxSum = currentSum;
+    }
+  }
+  return maxSum;
+}
 
 module.exports = maxSubarraySum;
