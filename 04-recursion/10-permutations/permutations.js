37235287f600a3fe384cfa10981c93838ef70855
James Njoroge <developerjaymmy@gmail.com>
2025-10-07 18:05:21 +0300

    39-permutations

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,23 @@
-function permutations() {}
+function permutations(str) {
+  let result = [];
+
+  if (str === "") {
+    result.push("");
+    return result;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const firstChar = str[i];
+    const restOfStr = str.slice(0, i) + str.slice(i + 1);
+
+    const subPermutation = permutations(restOfStr);
+
+    for (let j = 0; j < subPermutation.length; j++) {
+      result.push(firstChar + subPermutation[j]);
+    }
+  }
+
+  return result;
+}
 
 module.exports = permutations;
