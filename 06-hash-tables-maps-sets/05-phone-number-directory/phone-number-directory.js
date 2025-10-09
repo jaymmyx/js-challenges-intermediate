6474aa852941acd1d687345cfe149b34a06c8d8a
James Njoroge <developerjaymmy@gmail.com>
2025-10-09 08:17:57 +0300

    43-phone-number-directory

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,13 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(arr) {
+  const phoneNumDirectoryMap = new Map();
+
+  for (const item of arr) {
+    const [name, phone] = item.split(":");
+
+    phoneNumDirectoryMap.set(name, phone);
+  }
+
+  return phoneNumDirectoryMap;
+}
 
 module.exports = phoneNumberDirectory;
