ecea499226c9f9a8a6f2d45d29acc87d6092f1ff
James Njoroge <developerjaymmy@gmail.com>
2025-10-09 08:32:24 +0300

    44-anagram-grouping

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,19 @@
-function anagramGrouping() {}
+function anagramGrouping(words) {
+  const anagramGroupMap = new Map();
+
+  for (const word of words) {
+    const sortedWord = word.split("").sort().join();
+    if (anagramGroupMap.has(sortedWord)) {
+      anagramGroupMap.set(
+        sortedWord,
+        anagramGroupMap.get(sortedWord).concat(word)
+      );
+    } else {
+      anagramGroupMap.set(sortedWord, [word]);
+    }
+  }
+
+  return Array.from(anagramGroupMap.values())
+}
 
 module.exports = anagramGrouping;
