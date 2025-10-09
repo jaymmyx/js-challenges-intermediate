30b486b56934cae1b06cae51b42b3b427c049fb2
James Njoroge <developerjaymmy@gmail.com>
2025-10-09 08:07:31 +0300

    42-word-frequency-counter

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,19 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  const words = str.toLowerCase().split(/\W+/g);
+
+  const wordFreqMap = new Map();
+
+  for (const word of words) {
+    if (word === "") continue;
+
+    if (wordFreqMap.has(word)) {
+      wordFreqMap.set(word, wordFreqMap.get(word) + 1);
+    } else {
+      wordFreqMap.set(word, 1);
+    }
+  }
+
+  return wordFreqMap;
+}
 
 module.exports = wordFrequencyCounter;
