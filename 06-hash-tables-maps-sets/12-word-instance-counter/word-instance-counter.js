29dba846cd336386eb9a56ffdc48a1a8612c3f0e
James Njoroge <developerjaymmy@gmail.com>
2025-10-13 09:10:41 +0300

    50-custom-hash-table-problems

--- 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -1,5 +1,14 @@
-const HashTable = require('./HashTable');
+const HashTable = require("./HashTable");
 
-function wordInstanceCounter() {}
+function wordInstanceCounter(str, word) {
+  const strArr = str.toLowerCase().split(/\W+/g);
+  const wordFreqHash = new HashTable();
+
+  for (const word of strArr) {
+    wordFreqHash.set(word, (wordFreqHash.get(word) || 0) + 1);
+  }
+
+  return wordFreqHash.get(word);
+}
 
 module.exports = wordInstanceCounter;
--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,28 @@
-const HashTable = require('./HashTable');
+const HashTable = require("./HashTable");
 
-function anagramGrouping() {}
+function anagramGrouping(words) {
+  const anagramGroup = new HashTable();
+  const uniqueSortedWords = [];
+  const anagramArr = [];
+  for (const word of words) {
+    const sortedWord = word.toLowerCase().split("").sort().join("");
+
+    if (anagramGroup.has(sortedWord)) {
+      anagramGroup.set(
+        sortedWord,
+        anagramGroup.get(sortedWord).concat(...[word])
+      );
+    } else {
+      anagramGroup.set(sortedWord, [word]);
+      uniqueSortedWords.push(sortedWord);
+    }
+  }
+
+  for (const uniqueSortedWord of uniqueSortedWords) {
+    anagramArr.push(anagramGroup.get(uniqueSortedWord));
+  }
+
+  return anagramArr;
+}
 
 module.exports = anagramGrouping;
