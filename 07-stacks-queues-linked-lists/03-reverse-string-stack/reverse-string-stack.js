eb2ba17d6de43a33a20b32585309a00a02191959
James Njoroge <developerjaymmy@gmail.com>
2025-10-15 09:40:21 +0300

    53-reverse-string-stack

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,15 @@
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+  const strStack = new Stack();
+  let reversedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    strStack.push(str[i]);
+  }
+  for (let i = 0; i < str.length; i++) {
+    reversedStr += strStack.pop();
+  }
+  return reversedStr;
+}
 
 module.exports = reverseStringStack;
