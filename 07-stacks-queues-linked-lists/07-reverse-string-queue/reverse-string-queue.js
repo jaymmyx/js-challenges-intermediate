df260d245f56bef8d8984f143ec0bcd0fb348496
James Njoroge <developerjaymmy@gmail.com>
2025-10-16 05:16:01 +0300

    55-reverse-string-queue

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,19 @@
-const Queue = require('./queue');
+const Queue = require("./queue");
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+  const reverseQueue = new Queue();
+  let reversedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseQueue.enqueue(str[i]);
+  }
+  
+  while (!reverseQueue.isEmpty()) {
+    reversedStr += reverseQueue.peek();
+    reverseQueue.dequeue();
+  }
+
+  return reversedStr;
+};
 
 module.exports = reverseStringWithQueue;
