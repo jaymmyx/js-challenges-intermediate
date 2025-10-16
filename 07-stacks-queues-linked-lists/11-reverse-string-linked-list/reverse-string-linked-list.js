4602a7af669c5577c72181ce71bb83898dfa7b9d
James Njoroge <developerjaymmy@gmail.com>
2025-10-16 17:01:37 +0300

    reverse-string-linked-list

--- 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,18 @@
-const { LinkedList } = require('./linked-list');
+const { LinkedList } = require("./linked-list");
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(str) {
+  const linkedList = new LinkedList();
+  for (let i = str.length - 1; i >= 0; i--) {
+    const char = str.charAt(i);
+    linkedList.add(char);
+  }
+  let reversedString = "";
+  let current = linkedList.head;
+  while (current !== null) {
+    reversedString += current.data;
+    current = current.next;
+  }
+  return reversedString;
+}
 
 module.exports = reverseStringLinkedList;
