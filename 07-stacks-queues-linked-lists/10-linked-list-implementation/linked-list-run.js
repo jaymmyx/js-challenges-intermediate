9bd837ada76ed87f0711bc045eb7e99130eb8afb
James Njoroge <developerjaymmy@gmail.com>
2025-10-16 16:49:40 +0300

    58-linked-list-implementation

--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
@@ -1,1 +1,12 @@
-const { LinkedList } = require('./linked-list');
+const { LinkedList } = require("./linked-list");
+
+const list = new LinkedList();
+
+list.add(100);
+list.add(200);
+list.add(300);
+list.add(400);
+list.insertAt(3, 350);
+list.removeFrom(4)
+
+console.log(list.printAll());
